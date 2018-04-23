09d0ef7a5cb6473febe293ac9c3c4e29edeb3bad
Mohd Fahmi Anuar <homeprk@gmail.com>
2018-04-23 17:14:09 +0800

    Enabling Cross Origin Resource Sharing
    Enabling CORS
    
    Reff :
    https://www.codementor.io/chiemelachinedum/steps-to-enable-cors-on-a-lum
    en-api-backend-e5a0s1ecx

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -59,13 +59,16 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
+ $app->middleware([
 //    App\Http\Middleware\ExampleMiddleware::class
-// ]);
+     App\Http\Middleware\CorsMiddleware::class
+ ]);
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
 ]);
+
+
 
 /*
 |--------------------------------------------------------------------------
